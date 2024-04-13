2282fb05c8eb1c838022febe050912711ca50982
billxu1401 <billxu1401@gmail.com>
2024-04-12 20:46:52 -0400

    refined n-gram

--- markov_chain.py
+++ markov_chain.py
@@ -1,69 +1,26 @@
-# Markov chain is essentially just an N-gram
-
-'''
-Goals: we want to learn to make our own NLP program that is capable of generating more of
-or merging some of our favorite texts. In order to do this, we will first start with a 
-basic n-gram model. We will progressively add different context tensor data with the 
-goal of optimizing our model for this specific task and understanding our given text very 
-well. 
-
-Some possible vector matrix layers to add to our tensor:
-1. Word correlation within sentences/ association. Ex. Harry potter with magician, ron, hermione, (positive words too, Voldemort with negative) 
-2. Sentence-level "mood" - ex. urgent, inquisitive etc. (sliding window of mood values at both the sentece and word level for 
-understanding the context a feel of the story)
-3. Paragraph/story level context - Exposition, rising action, climax, falling action, resolution
-4. Frequency of following words (n-gram)
-5. Current sentence length (we don't want run ons)
-6. Keep track of exising noun words in action? (For instance we don't want random objects just appearing we want it to be realistic)
-
-
-'''
-
 import nltk
 import random
 
-with open("./Prisoner_of_Azkaban.txt", "r") as f: # update the path accordingly.
-    data = f.read()
-
+# split the corpus into sentences based on period and line break
 def split_to_sentences(data):
-    return [sentence for sentence in map(str.strip, data.split('.')) if sentence] # split based on period and line break
+    return [sentence for sentence in map(str.strip, data.split('.')) if sentence]
 
+
+# tokenize the sentences and remove unwanted punctuations from the tokens
 def tokenize_sentences(sentences):
-    """
-    Tokenize sentences into tokens (words)
-    
-    Args:
-        sentences: List of strings
-    
-    Returns:
-        List of lists of tokens
-    """
+    unwanted_punct = ['``', '--', '\'\'']
     
     tokenized_sentences = []
     for s in sentences:
         s = s.lower()
         tokens = nltk.word_tokenize(s)
+        tokens = list(filter(lambda x: x not in unwanted_punct, tokens))
         tokenized_sentences.append(tokens)
     
     return tokenized_sentences
 
-sentences = tokenize_sentences(split_to_sentences(data))
 
-# Generate n-grams of size n
-# sentences: list of sentence to generate n-gram from
-# n: size of each sequence
-def generate_ngram(sentences, n):
-    ngram = []
-    for s in sentences:
-        ngram.append([s[i:i+n] for i in range(len(s)-n+1)])
-    return ngram
-
-three_gram = generate_ngram(sentences, 3)
-# print(three_gram)
-# for gram in three_gram:
-#     print(next(gram))
-
-
+# create a model of possible subsequent words of each word
 def possible_next_words(n, sentences):
     model = {}
     for s in sentences:
@@ -76,21 +33,58 @@ def possible_next_words(n, sentences):
             model[ngram].append(next)
     return model
 
-model = possible_next_words(3, sentences)
 
-
-def generate_from_model(model, n, start=None, max_words=1000):
-    if not start:
+# generate sentences based on a model using n-grams with size n and up to max_words amount of words
+def generate_from_model(model, n, max_words):
+    # randomly choose a start n-gram
+    start = random.choice(list(model.keys()))
+    while(not start[0].isalpha()):
+        # make sure sentence starts with an English word
         start = random.choice(list(model.keys()))
+
     result = list(start)
     for i in range(max_words):
         start = tuple(result[-n:])
         next = random.choice(model[start])
         if not next:
            result[-1] += '.' # end of sentence, start new ngram
-           result.extend(random.choice(list(model.keys())))
+           start = random.choice(list(model.keys()))
+           while(not start[0].isalpha()):
+               start = random.choice(list(model.keys()))
+           result.extend(start)
         else:
             result.append(next)
-    return ' '.join(result)
+    return ' '.join(join_punctuation(result))
 
-print(generate_from_model(model, 3))
+
+# join punctuations with previous word in a list
+def join_punctuation(seq):
+    characters = set('.,;?!')
+    seq = iter(seq)
+    current = next(seq)
+
+    for nxt in seq:
+        if nxt in characters:
+            current += nxt
+        else:
+            yield current
+            current = nxt
+
+    yield current
+
+
+# print out a story combining corpus of text1, text2, using n-gram with size n and up to max_words amount of words
+def generate_story(text1, text2, n, max_words):
+    with open(text1, "r", encoding="latin-1") as f1:
+        data1 = f1.read()    
+    with open(text2, "r", encoding="latin-1") as f2:
+        data2 = f2.read()
+
+    sentence_splits = split_to_sentences(data1)
+    sentence_splits.extend(split_to_sentences(data2))
+    sentences = tokenize_sentences(sentence_splits)
+    model = possible_next_words(n, sentences)
+    print(generate_from_model(model, n, max_words))
+
+
+generate_story("./Prisoner_of_Azkaban.txt", "./atlasshrugged.txt", 2, 1000)
