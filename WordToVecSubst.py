46706c492c9922a113f29c2419b04dbcd8084bd6
Jake Writer <writer.j@northeastern.edu>
2024-04-17 15:15:39 -0400

    minor

--- WordToVecSubst.py
+++ WordToVecSubst.py
@@ -15,18 +15,21 @@ with open('macbeth.txt') as f:
 macbeth_tokens = word_tokenize(macbeth_text.lower())
 
 macbeth_vectors = {word: model[word] for word in macbeth_tokens if word in model.key_to_index}
+print(macbeth_vectors.keys())
 
 def find_similar_shakespeare(word, model, shakespeare_vectors):
     if word in model.key_to_index:
         # Find most similar words according to the model
         similarities = model.most_similar(positive=[model[word]], topn=10)
         # Filter to find the most similar word that is also in Macbeth's vector space
+        print(similarities)
         similar_words = [sim_word for sim_word, _ in similarities if sim_word in shakespeare_vectors]
+        # print(similar_words)
         if similar_words:
             return similar_words[0]  # Return the most similar word found in Macbeth
     return word  # Return the original word if no similar word is found
 
-with open('HarryPotterSection.txt') as f:
+with open('AtlasShruggedSection.txt') as f:
     other_text = f.read()
 other_tokens = word_tokenize(other_text.lower())
 
